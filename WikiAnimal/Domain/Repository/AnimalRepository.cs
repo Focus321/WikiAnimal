6dd047b9ef23170fb602f700d534be39d18f468f
Nick Bachurin <43537358+nickdpua@users.noreply.github.com>
2020-09-07 14:47:52 +0300

    update AnimalRepository
    update AnimalRepository

--- WikiAnimal/Domain/Repository/AnimalRepository.cs
+++ WikiAnimal/Domain/Repository/AnimalRepository.cs
@@ -20,6 +20,24 @@ namespace WikiAnimal.Domain.Repository
 
         private AnimalDatabaseContext Context { get; }
 
+        public async Task Add(Animal obj)
+        {
+            await Context.Animals.AddAsync(obj);
+        }
+
+        public async Task Change(Animal obj)
+        {
+            var animal = (await Context.Animals.FirstOrDefaultAsync(x=>x.Id == obj.Id));
+            if (animal != null)
+            {
+                animal.Name = animal.Name;
+                animal.Habitat = animal.Habitat;
+                animal.ImagePath = animal.ImagePath;
+                animal.ShortDescription = animal.ShortDescription;
+                animal.Description = animal.Description;
+                animal.Appearance = animal.Appearance;
+            }
+        }
 
         public async Task<IReadOnlyCollection<Animal>> FindByConditionAsync(Expression<Func<Animal, bool>> predicat)
         {
