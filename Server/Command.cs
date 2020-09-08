78e0ac7de938176177c9020864c299d02517b18d
Kuzmin00 <43570410+Kuzmin00@users.noreply.github.com>
2020-09-08 14:30:44 +0300

    Add TCPServer and ChatService
    Add TCPServer and ChatService

---
+++ Server/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    public class Command
+    {
+        public string Message { get; set; }
+        public string Nickname { get; set; }
+    }
+}
